c0819997260028bb02ccd44d1a765947f4ea4a9a
Lepadatu Emanuel <emanuel.lepadatu04@e-uvt.ro>
2024-10-21 18:47:06 +0300

    Add view engine

--- app.js
+++ app.js
@@ -2,25 +2,24 @@ const express = require('express');
 
 const app = express();
 
+//register view eengine
+app.set('view engine','ejs');
+app.set('views','frontend');
+
+
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    //res.send('<p> Home page </p>');
-    res.sendFile('./frontend/index.html',{root:__dirname});
+    res.render('index',{title: 'Home'});
 });
 app.get('/about', (req,res)=>{
-    //res.send('<p>About page </p>');
-    res.sendFile('./frontend/about.html',{root:__dirname});
-
+    res.render('about',{title: 'About'});
 });
 
-//redirects
-
-app.get('about-us',(req,res)=>{
-    res.redirect('about');
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title: 'Create'});
 });
 
-//404 page
 app.use((req,res)=>{
-    res.status(404).sendFile('./frontend/404.html',{root:__dirname})
+    res.status(404).render('404',{title: '404'});
 });
