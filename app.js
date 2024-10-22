ec793c8d505218f813926236f5d8b938ec28b4fe
Lepadatu Emanuel <lepadatuemanuel7@gmail.com>
2024-10-22 18:48:04 +0300

    server to frontend

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.set('views','frontend');
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.render('index',{title: 'Home'});
+    const blogs = [
+    {title:'Blog nr 1', snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue mollis sapien placerat tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae leo nec metus molestie pretium at vitae massa. Cras elit purus, convallis et tempus non, varius congue ligula. Quisque nec velit viverra, sagittis sapien vitae, pharetra tortor. Duis hendrerit augue eu lectus pellentesque egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'},
+    {title:'Blog nr 2', snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue mollis sapien placerat tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae leo nec metus molestie pretium at vitae massa. Cras elit purus, convallis et tempus non, varius congue ligula. Quisque nec velit viverra, sagittis sapien vitae, pharetra tortor. Duis hendrerit augue eu lectus pellentesque egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'},
+    {title:'Blog nr 3', snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue mollis sapien placerat tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vitae leo nec metus molestie pretium at vitae massa. Cras elit purus, convallis et tempus non, varius congue ligula. Quisque nec velit viverra, sagittis sapien vitae, pharetra tortor. Duis hendrerit augue eu lectus pellentesque egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'},
+    ];
+    res.render('index',{title: 'Home',blogs:blogs});
 });
 app.get('/about', (req,res)=>{
     res.render('about',{title: 'About'});
